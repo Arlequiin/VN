d9f644fcbec15a06ed74f67b09881d8b40f0018c
αρλ <78297845+Arlequiin@users.noreply.github.com>
2023-01-06 23:39:06 +0000

    sprite command

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import discord
 import os # default module
 from dotenv import load_dotenv
 bot = discord.Bot()
-@bot.command(description="Sends the bot's latency.") 
+@bot.command(description="Envoie la latence du bot") 
 async def ping(ctx):
-    await ctx.respond(f"Hello world")
+    await ctx.respond(f"Pong! {bot.latency}")
+@bot.command(description="Envoie le sprite d'un Pokémon") 
+async def sprite(ctx,pokemon):
+    await ctx.respond(f"https://raw.githubusercontent.com/Arlequiin/pokeemerald-expansion/master/graphics/pokemon/{pokemon}/front.png")
 keep_alive()
-bot.run(Token)
+bot.run(os.getenv("TOKEN"))
